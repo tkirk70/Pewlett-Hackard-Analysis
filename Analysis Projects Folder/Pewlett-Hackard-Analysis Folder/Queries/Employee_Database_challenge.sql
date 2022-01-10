076da0bed0376778c0fd1acf7fe62f6fb816cfa7
tkirk70 <timothypatrickkirk@gmail.com>
2022-01-10 08:58:06 -0500

    Adding average age for deliverable #3 analysis.

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -51,3 +51,45 @@ FROM unique_titles u
 FULL OUTER JOIN dept_emp de
 ON u.emp_no = de.emp_no
 WHERE to_date = '9999-01-01';
+
+-- Figure out how many mentors will be available for potential new hires.
+SELECT COUNT(u.emp_no) AS soon_to_retire, COUNT(me.emp_no) AS mentors,
+ROUND(COUNT(u.emp_no) * 1.0/COUNT(me.emp_no), 2) AS mentee_per_mentor
+FROM unique_titles u
+FULL OUTER JOIN mentorship_eligibility me
+ON u.emp_no = me.emp_no;
+
+-- Figure out the ages of potential retirees.
+SELECT *, DATE_PART('year', '2022-01-10'::date) - DATE_PART('year', e.birth_date::date) AS age
+FROM employees e
+JOIN unique_titles u
+ON u.emp_no = e.emp_no;
+
+-- Avg age - How to work with ROUND and Double Precision?
+SELECT AVG(DATE_PART('year', '2022-01-10'::date) - DATE_PART('year', e.birth_date::date)) AS avg_age
+FROM employees e
+JOIN unique_titles u
+ON u.emp_no = e.emp_no;
+
+-- Avg Age of Remaining employees
+SELECT COUNT(DISTINCT(t.emp_no)),
+AVG(DATE_PART('year', '2022-01-10'::date) - DATE_PART('year', e.birth_date::date))
+AS avg_age
+FROM titles t
+JOIN employees e
+ON e.emp_no = t.emp_no
+WHERE e.birth_date > '1955-12-31'
+AND t.to_date = '9999-01-01'; 
+
+-- Salaries (retiring)
+SELECT count(*) AS number_retirees, ROUND(AVG(s.salary), 2) AS average_salary
+FROM salaries s
+JOIN public.unique_titles u
+ON u.emp_no = s.emp_no;
+
+-- Salaries total
+SELECT COUNT(*) AS current_employees, ROUND(AVG(s.salary), 2) AS average_salary
+FROM dept_emp de
+JOIN salaries s
+ON s.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01';
