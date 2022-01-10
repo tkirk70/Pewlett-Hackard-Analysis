5e4456b6d27180cd6608c38c7d5a684ebc45173a
tkirk70 <timothypatrickkirk@gmail.com>
2022-01-09 19:10:15 -0500

    Module 7 Challenge.

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -6,13 +6,11 @@ FROM employees AS e
 INNER JOIN titles AS t
 ON t.emp_no = e.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
--- AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
--- AND t.to_date = ('9999-01-01')
 ORDER BY e.emp_no;
 
 -- Copy/paste Employee_Challenge_starter_code.sql
 
--- Use Dictinct with Order By to remove duplicate rows
+-- Use Dictinct ON() with Order By to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
   first_name,
   last_name,
@@ -22,6 +20,7 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
+-- Count potential retirees and groub by title.
 SELECT count(title), title
 INTO retiring_titles
 FROM public.unique_titles
@@ -29,7 +28,10 @@ GROUP BY title
 ORDER BY count(title) DESC;
 
 -- Deliverable #2
-SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+-- Create a mentorship-eligibility table that holds the current employees who
+-- were born between January 1, 1965 and December 31, 1965.
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name,
+  e.birth_date, de.from_date, de.to_date, t.title
 INTO mentorship_eligibility
 FROM employees AS e
 JOIN dept_emp AS de
